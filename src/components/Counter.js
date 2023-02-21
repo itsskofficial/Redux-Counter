248444549a1b3e24d4e611a674e8373770af9353
SK <sarthakkarandikar03@gmail.com>
2023-02-21 00:12:11 +0000

    Feb 21, 2023, 5:42 AM

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -9,6 +9,9 @@ const Counter = () => {
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
       <div className={classes.value}>{counter}</div>
+      <div>
+        <button>Increment</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
