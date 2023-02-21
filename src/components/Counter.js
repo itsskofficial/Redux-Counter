5a0df539c460d48c50ca14097a6065a987e8c29c
SK <sarthakkarandikar03@gmail.com>
2023-02-21 11:18:03 +0000

    Feb 21, 2023, 4:48 PM

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -3,6 +3,7 @@ import classes from './Counter.module.css';
 
 const Counter = () => {
   const counter = useSelector(state=>state.counter)
+  cosn
   const dispatch = useDispatch()
   const toggleCounterHandler = () => {
     dispatch({type:'toggle'})
