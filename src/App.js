28c05e628f034ed6fe21b606df44ea94f950fbfb
SK <sarthakkarandikar03@gmail.com>
2023-02-22 00:15:25 +0000

    Feb 22, 2023, 5:45 AM

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import Auth from './components/Auth';
 import UserProfile from './components/UserProfile'
 import { useSelector } from 'react-redux';
 import { Fragment } from 'react';
+import Header from './components/Header';
 
 function App() {
   const isAuthenticated = useSelector(state => state.auth.isAuthenticated)
   return (
     <Fragment>
-      {is}
+      {isAuthenticated && <Header/>}
       {isAuthenticated ? <UserProfile /> : <Auth/>}
       <Counter />
     </Fragment>
