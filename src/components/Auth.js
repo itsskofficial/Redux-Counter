f1304b1ecac14fb40943f9aa6d67e142c0249a2f
SK <sarthakkarandikar03@gmail.com>
2023-02-22 00:04:15 +0000

    Feb 22, 2023, 5:34 AM

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -22,7 +22,7 @@ const Auth = () => {
             <label htmlFor='password'>Password</label>
             <input type='password' id='password' />
           </div>
-          <button onClick={loginHan}>Login</button>
+          <button onClick={loginHandler}>Login</button>
         </form>
       </section>
     </main>
