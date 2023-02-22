89df3faf3f0d9223b234e11cb308ed89728c31a1
SK <sarthakkarandikar03@gmail.com>
2023-02-22 00:20:15 +0000

    Feb 22, 2023, 5:50 AM

--- src/store/counter.js
+++ src/store/counter.js
@@ -1,1 +1,25 @@
-import createSlice
+import { createSlice } from "@reduxjs/toolkit";
+
+const counterSlice = createSlice({
+    name: 'counter',
+    initialState:{ counter: 0, showCounter: true },
+    reducers: {
+        increment(state) {
+            state.counter++
+        },
+
+        decrement(state) {
+            state.counter--
+        },
+
+        increase(state, action) {
+            state.counter+=action.payload
+        },
+
+        toggle(state) {
+            state.showCounter=!state.showCounter
+        }
+    }
+})
+
+export 
