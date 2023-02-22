ead937acf1dc9809dc717800297c6de1419e63e8
SK <sarthakkarandikar03@gmail.com>
2023-02-22 00:09:36 +0000

    Feb 22, 2023, 5:39 AM

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import Counter from './components/Counter';
-
+import { useSelector } from 'react-redux';
 
 function App() {
+  const isAuthenticated = useSelector(state => state.auth.isAuthenticated)
   return (
     <Counter />
   );
