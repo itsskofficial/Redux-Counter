a796e73780f6106472799837e9a3fdc92aa74ad6
SK <sarthakkarandikar03@gmail.com>
2023-02-22 00:10:52 +0000

    Feb 22, 2023, 5:40 AM

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   const isAuthenticated = useSelector(state => state.auth.isAuthenticated)
   return (
     <Fragment>
-      {isAuthenticated && <Auth />}
+      {isAuthenticated && <Auth /> || <UserPr}
       <Counter />
   );
 }
