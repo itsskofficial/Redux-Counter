ebacb5cbd931b591603244a42a53e0508428130e
SK <sarthakkarandikar03@gmail.com>
2023-02-22 00:21:43 +0000

    Feb 22, 2023, 5:51 AM

--- src/store/auth.js
+++ src/store/auth.js
@@ -1,1 +1,17 @@
-import createSlice
+import { createSlice } from "@reduxjs/toolkit";
+
+const authSlice = createSlice({
+    name: 'auth',
+    initialState: {isAuthenticated:false},
+    reducers: {
+        login(state){
+            state.isAuthenticated = false
+        },
+        
+        logout(state) {
+            state.isAuthenticated=true
+        }
+    }
+})
+
+export default authSlice
